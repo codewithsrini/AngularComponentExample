5e4c0f05f0574908e01d33cdb7f0e6a420c82642
Srini <42915005+codewithsrini@users.noreply.github.com>
2018-11-18 22:00:57 +0530

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BooksComponent } from './books/books.component';
+import { MainNavComponent } from './main-nav/main-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BooksComponent,
+    MainNavComponent
   ],
   imports: [
     BrowserModule
